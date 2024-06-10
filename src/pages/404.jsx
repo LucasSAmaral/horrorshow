234c4a7fed96620a0b8339ab5d915f54eac702b1
Lucas Amaral <lucas.samaral@b2wdigital.com>
2024-06-10 12:41:04 -0300

    ajusta a imagem na página de 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,46 +1,60 @@
-import * as React from "react"
-import { graphql } from "gatsby"
+import * as React from "react";
+import { graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import { StaticImage } from "gatsby-plugin-image"
-import styled from "styled-components"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import { StaticImage } from "gatsby-plugin-image";
+import styled from "styled-components";
 
 const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.nodes
+  const siteTitle = data.site.siteMetadata.title;
+  const posts = data.allMarkdownRemark.nodes;
 
   return (
     <Layout posts={posts} location={location} title={siteTitle}>
       <Wrapper>
         <h1>404: Nada pra ver aqui</h1>
         <StaticImage
-          layout="fixed"
+          layout="constrained"
           formats={["auto", "webp", "avif"]}
           src="../images/blood-hand.png"
+          width={400}
           quality={95}
           alt="Mancha de sangue em forma de mão"
         />
       </Wrapper>
     </Layout>
-  )
-}
+  );
+};
 
-export const Head = () => <Seo title="404" />
+export const Head = () => <Seo title="404" />;
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  @media (max-width: 1180px) {
+    text-align: center;
+  }
 
   h1 {
     font-family: var(--title-font);
     color: var(--title-color);
     font-size: 55px;
     margin: 0 0 35px 0;
-  }
-`
 
-export default NotFoundPage
+    @media (max-width: 1180px) {
+      font-size: 45px;
+    }
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+  }
+`;
+
+export default NotFoundPage;
 
 export const pageQuery = graphql`
   query {
@@ -60,4 +74,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
