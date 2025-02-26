274300b7856f30cd4315075803544d783d76023c
Lucas Amaral <lucas.samaral@b2wdigital.com>
2025-02-26 00:58:19 -0300

    ajusta a query no gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,128 +18,128 @@ const authorsTemplate = path.resolve(`./src/templates/authors.jsx`);
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions;
+	const { createPage } = actions;
 
-  // Get all markdown blog posts sorted by date
-  const result = await graphql(`
+	// Get all markdown blog posts sorted by date
+	const result = await graphql(`
     {
-      allMarkdownRemark(sort: { frontmatter: { date: ASC } }, limit: 1000) {
-        nodes {
-          id
-          frontmatter {
-            title
-            description
-            date
-          }
-          fields {
-            slug
-          }
-        }
-      }
-      tagsGroup: allMarkdownRemark(limit: 2000) {
-        group(field: { frontmatter: { tags: SELECT } }) {
-          fieldValue
-        }
-      }
-      authorsGroup: allMarkdownRemark(limit: 2000) {
-        group(field: { frontmatter: { author: SELECT } }) {
-          fieldValue
+  allContentfulPost(sort: {date: ASC}, limit: 1000) {
+    edges {
+      node {
+        id
+        title
+        slug
+        date
+        description {
+          description
         }
       }
     }
+  }
+  tagsGroup: allContentfulPost (limit: 2000) {
+    group(field: {tags: {tags: SELECT}}) {
+      fieldValue
+    }
+  }
+  authorsGroup: allContentfulPost(limit: 2000) {
+    group(field: {author: SELECT}) {
+      fieldValue
+    }
+  }
+}
   `);
 
-  if (result.errors) {
-    reporter.panicOnBuild(
-      `There was an error loading your blog posts`,
-      result.errors
-    );
-    return;
-  }
+	if (result.errors) {
+		reporter.panicOnBuild(
+			`There was an error loading your blog posts`,
+			result.errors,
+		);
+		return;
+	}
 
-  const posts = result.data.allMarkdownRemark.nodes;
+	const posts = result.data.allContentfulPost.edges;
 
-  // Create blog posts pages
-  // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
-  // `context` is available in the template as a prop and as a variable in GraphQL
+	// Create blog posts pages
+	// But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
+	// `context` is available in the template as a prop and as a variable in GraphQL
 
-  if (posts.length > 0) {
-    posts.forEach((post, index) => {
-      const previousPostId = index === 0 ? null : posts[index - 1].id;
-      const nextPostId =
-        index === posts.length - 1 ? null : posts[index + 1].id;
+	if (posts.length > 0) {
+		posts.forEach((post, index) => {
+			const previousPostId = index === 0 ? null : posts[index - 1].id;
+			const nextPostId =
+				index === posts.length - 1 ? null : posts[index + 1].id;
 
-      createPage({
-        path: post.fields.slug,
-        component: blogPost,
-        context: {
-          id: post.id,
-          previousPostId,
-          nextPostId,
-        },
-      });
-    });
-  }
+			createPage({
+				path: post.node.slug,
+				component: blogPost,
+				context: {
+					id: post.node.id,
+					previousPostId,
+					nextPostId,
+				},
+			});
+		});
+	}
 
-  const tags = result.data.tagsGroup.group;
+	const tags = result.data.tagsGroup.group;
 
-  tags.forEach(tag => {
-    const kebabCaseTag = _.kebabCase(tag.fieldValue);
+	tags.forEach((tag) => {
+		const kebabCaseTag = _.kebabCase(tag.fieldValue);
 
-    createPage({
-      path: `/tag/${kebabCaseTag}/`,
-      component: tagsTemplate,
-      context: {
-        tag: tag.fieldValue,
-      },
-    });
-  });
+		createPage({
+			path: `/tag/${kebabCaseTag}/`,
+			component: tagsTemplate,
+			context: {
+				tag: tag.fieldValue,
+			},
+		});
+	});
 
-  const authors = result.data.authorsGroup.group;
+	const authors = result.data.authorsGroup.group;
 
-  authors.forEach(author => {
-    const kebabCaseAuthor = _.kebabCase(author.fieldValue);
+	authors.forEach((author) => {
+		const kebabCaseAuthor = _.kebabCase(author.fieldValue);
 
-    createPage({
-      path: `/author/${kebabCaseAuthor}/`,
-      component: authorsTemplate,
-      context: {
-        author: author.fieldValue,
-      },
-    });
-  });
+		createPage({
+			path: `/author/${kebabCaseAuthor}/`,
+			component: authorsTemplate,
+			context: {
+				author: author.fieldValue,
+			},
+		});
+	});
 };
 
 /**
  * @type {import('gatsby').GatsbyNode['onCreateNode']}
  */
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
+	const { createNodeField } = actions;
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
+	if (node.internal.type === `MarkdownRemark`) {
+		const value = createFilePath({ node, getNode });
 
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    });
-  }
+		createNodeField({
+			name: `slug`,
+			node,
+			value,
+		});
+	}
 };
 
 /**
  * @type {import('gatsby').GatsbyNode['createSchemaCustomization']}
  */
 exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions;
+	const { createTypes } = actions;
 
-  // Explicitly define the siteMetadata {} object
-  // This way those will always be defined even if removed from gatsby-config.js
+	// Explicitly define the siteMetadata {} object
+	// This way those will always be defined even if removed from gatsby-config.js
 
-  // Also explicitly define the Markdown frontmatter
-  // This way the "MarkdownRemark" queries will return `null` even when no
-  // blog posts are stored inside "content/blog" instead of returning an error
-  createTypes(`
+	// Also explicitly define the Markdown frontmatter
+	// This way the "MarkdownRemark" queries will return `null` even when no
+	// blog posts are stored inside "content/blog" instead of returning an error
+	createTypes(`
     type SiteSiteMetadata {
       author: [Author]
       siteUrl: String
