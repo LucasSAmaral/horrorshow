2a056878c8dcb3832bda54172eec3c6afc3c40a9
Lucas Amaral <lucas.samaral@b2wdigital.com>
2025-02-26 00:43:32 -0300

    instala gatsby-source-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+	path: `.env.${process.env.NODE_ENV}`,
 });
 
 /**
@@ -12,130 +12,137 @@ require("dotenv").config({
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Horrorshow`,
-    author: [
-      {
-        name: `Lucas Amaral`,
-        summary: [
-          `Tinha medo de filmes de terror quando criança. Hoje é o gênero que ele mais assiste.`,
-          `Desenvolvedor Front-End de profissão. Crítico amador de cinema
+	siteMetadata: {
+		title: `Horrorshow`,
+		author: [
+			{
+				name: `Lucas Amaral`,
+				summary: [
+					`Tinha medo de filmes de terror quando criança. Hoje é o gênero que ele mais assiste.`,
+					`Desenvolvedor Front-End de profissão. Crítico amador de cinema
                   nas horas vagas.`,
-        ],
-        social: {
-          letterboxd: `http://letterboxd.com/lucas3001`,
-        },
-      },
-      {
-        name: `Gilma G.`,
-        summary: [
-          `Apreciadora de arte e cultura, curiosa, corajosa e nas horas vagas, ouvinte.`,
-        ],
-      },
-      {
-        name: `Deborah Gonçalves`,
-        summary: [
-          `Bibliotecária apaixonada por Cinema, intermediando sempre que possível, o melhor dos dois mundos.`,
-        ],
-        social: {
-          letterboxd: `http://letterboxd.com/dgoncalves_s`,
-        },
-      },
-    ],
-    description: `Falamos sobre terror e suspense de forma descontraída. Seja sobre filmes, séries, hqs e games. Até mesmo novelas. Por que não?`,
-    siteUrl: `http://www.horrorshow.com.br`,
-    social: {
-      instagram: "https://www.instagram.com/horrorshowbr",
-      twitter: "https://x.com/horrorshow_br",
-      tiktok: "https://www.tiktok.com/@horrorshowbr",
-      youtube: "https://www.youtube.com/@horrorshowbr",
-    },
-  },
-  plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          process.env.GA_ID, // Google Analytics / GA
-        ],
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-adsense`,
-      options: {
-        publisherId: "ca-pub-1907274240349428",
-      },
-    },
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
-        // Add any options here
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-disqus`,
-      options: {
-        shortname: `horrorshow-1`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 630,
-            },
-          },
-          {
-            resolve: "gatsby-remark-embed-youtube",
-            options: {
-              width: 800,
-              height: 400,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
+				],
+				social: {
+					letterboxd: `http://letterboxd.com/lucas3001`,
+				},
+			},
+			{
+				name: `Gilma G.`,
+				summary: [
+					`Apreciadora de arte e cultura, curiosa, corajosa e nas horas vagas, ouvinte.`,
+				],
+			},
+			{
+				name: `Deborah Gonçalves`,
+				summary: [
+					`Bibliotecária apaixonada por Cinema, intermediando sempre que possível, o melhor dos dois mundos.`,
+				],
+				social: {
+					letterboxd: `http://letterboxd.com/dgoncalves_s`,
+				},
+			},
+		],
+		description: `Falamos sobre terror e suspense de forma descontraída. Seja sobre filmes, séries, hqs e games. Até mesmo novelas. Por que não?`,
+		siteUrl: `http://www.horrorshow.com.br`,
+		social: {
+			instagram: "https://www.instagram.com/horrorshowbr",
+			twitter: "https://x.com/horrorshow_br",
+			tiktok: "https://www.tiktok.com/@horrorshowbr",
+			youtube: "https://www.youtube.com/@horrorshowbr",
+		},
+	},
+	plugins: [
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: process.env.CONTENTFUL_SPACE_ID,
+				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+			},
+		},
+		`gatsby-plugin-image`,
+		{
+			resolve: `gatsby-plugin-google-gtag`,
+			options: {
+				// You can add multiple tracking ids and a pageview event will be fired for all of them.
+				trackingIds: [
+					process.env.GA_ID, // Google Analytics / GA
+				],
+				// This object is used for configuration specific to this plugin
+				pluginConfig: {
+					// Puts tracking script in the head instead of the body
+					head: true,
+					// Setting this parameter is also optional
+					respectDNT: true,
+					// Avoids sending pageview hits from custom paths
+					exclude: ["/preview/**", "/do-not-track/me/too/"],
+				},
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-adsense`,
+			options: {
+				publisherId: "ca-pub-1907274240349428",
+			},
+		},
+		{
+			resolve: `gatsby-plugin-styled-components`,
+			options: {
+				// Add any options here
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/blog`,
+				name: `blog`,
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-disqus`,
+			options: {
+				shortname: `horrorshow-1`,
+			},
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 630,
+						},
+					},
+					{
+						resolve: "gatsby-remark-embed-youtube",
+						options: {
+							width: 800,
+							height: 400,
+						},
+					},
+					{
+						resolve: `gatsby-remark-responsive-iframe`,
+						options: {
+							wrapperStyle: `margin-bottom: 1.0725rem`,
+						},
+					},
+					`gatsby-remark-prismjs`,
+				],
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-feed`,
+			options: {
+				query: `
           {
             site {
               siteMetadata {
@@ -147,20 +154,20 @@ module.exports = {
             }
           }
         `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map(node => {
-                return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
-                });
-              });
-            },
-            query: `{
+				feeds: [
+					{
+						serialize: ({ query: { site, allMarkdownRemark } }) => {
+							return allMarkdownRemark.nodes.map((node) => {
+								return Object.assign({}, node.frontmatter, {
+									description: node.excerpt,
+									date: node.frontmatter.date,
+									url: site.siteMetadata.siteUrl + node.fields.slug,
+									guid: site.siteMetadata.siteUrl + node.fields.slug,
+									custom_elements: [{ "content:encoded": node.html }],
+								});
+							});
+						},
+						query: `{
               allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
                 nodes {
                   excerpt
@@ -176,25 +183,25 @@ module.exports = {
                 }
               }
             }`,
-            output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Horrorshow`,
-        short_name: `Horrorshow`,
-        start_url: `/`,
-        background_color: `#282c34`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/blood-hand.png`, // This path is relative to the root of the site.
-      },
-    },
-  ],
+						output: "/rss.xml",
+						title: "Gatsby Starter Blog RSS Feed",
+					},
+				],
+			},
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Horrorshow`,
+				short_name: `Horrorshow`,
+				start_url: `/`,
+				background_color: `#282c34`,
+				// This will impact how browsers show your PWA/website
+				// https://css-tricks.com/meta-theme-color-and-trickery/
+				// theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/blood-hand.png`, // This path is relative to the root of the site.
+			},
+		},
+	],
 };
